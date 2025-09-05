2854f7b13a7df6ad47d1bcf88e0bd2bb91dd707c
MikolajKos <niko.kosiorek@gmail.com>
2025-09-05 20:42:55 +0200

    delta time implemented

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -6,7 +6,9 @@ Particle::Particle(float pos_x, float pos_y, float vel_x, float vel_y, float rad
 	pos.y = pos_y;
 	vel.x = vel_x;
 	vel.y = vel_y;
-	
+
+	clock.start();
+
 	particle.setRadius(radius);
 	particle.setPosition(pos);
 	particle.setOrigin({ radius, radius });
@@ -18,7 +20,7 @@ void Particle::render(sf::RenderWindow& window) {
 	window.draw(particle);
 }
 
-void Particle::update_physics(const GravitySource &s) {
+void Particle::update_physics(const GravitySource& s) {
 	float dist_x = s.get_position().x - pos.x;
 	float dist_y = s.get_position().y - pos.y;
 	float dist = sqrt(dist_x * dist_x + dist_y * dist_y);
@@ -33,10 +35,25 @@ void Particle::update_physics(const GravitySource &s) {
 	float acceler_x = norm_x * s.get_strength() * inverse_square_dropoff;
 	float acceler_y = norm_y * s.get_strength() * inverse_square_dropoff;
 
-	vel.x += acceler_x;
-	vel.y += acceler_y;
+	float dt = clock.restart().asSeconds() * 100; // * 10 to slightly increase animation speed 
+	vel.x += acceler_x * dt;
+	vel.y += acceler_y * dt;
 
-	pos += vel;
+	pos += vel * dt;
+
+	/*
+	Comment:
+		This code introduces an artificial delay every 100 updates.
+		Without using dt, the animation "lags" and particles start from the same position after each refresh,
+		because movement is not scaled by real elapsed time.
+		When dt (the real time between frames) is used, particle motion corresponds to actual elapsed time,
+		so after pausing and resuming the animation, particles continue from the position consistent with the total simulation time.
+
+	Code:
+		static int count = 0;
+		if (++count % 100 == 0)
+			std	::this_thread::sleep_for(std::chrono::seconds(2));
+	*/
 }
 
 sf::Vector2f Particle::get_position() const { return pos; }
--- src/includes/Particle.hpp
+++ src/includes/Particle.hpp
@@ -2,12 +2,14 @@
 #define PARTICLE_HPP
 
 #include <SFML/Graphics.hpp>
+#include <thread>
 #include "GravitySource.hpp"
 
 class Particle {
 	sf::Vector2f pos;
 	sf::Vector2f vel;
 	sf::CircleShape particle;
+	sf::Clock clock;
 	float radius;
 public:
 	Particle(float pos_x, float pos_y, float vel_x, float vel_y, float radius);
